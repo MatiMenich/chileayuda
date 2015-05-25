6bc29ae9f74b571af645dcf38d116d6e217dc630
Claudin <claudio.berroeta@ing.uchile.cl>
2015-05-25 17:10:32 -0300

    se completa formulario. Se agregan las marcas a la db

--- maps/forms.py
+++ maps/forms.py
@@ -4,5 +4,5 @@ from .models import *
 class MarkForm(forms.Form):
     description = forms.CharField(widget=forms.TextInput, max_length=100)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Seleccione una categoría", widget=forms.Select(attrs={'class':'form-control input-sm'}))
-    latitud = forms.FloatField(required=False)
-    longitud = forms.FloatField(required=False)
+    latitud = forms.FloatField(widget=forms.TextInput(attrs={'id': 'latitud','type': 'hidden'}))
+    longitud = forms.FloatField(widget=forms.TextInput(attrs={'id': 'longitud','type': 'hidden'}))
