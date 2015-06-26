f02dd0ed83834755b71a64e7e12d39a2f583bc81
JoseTapiaG <jose.wt@gmail.com>
2015-06-26 01:10:53 -0300

    Objeto MapHandler Javascript Se crea objeto para manejar los datos de una catastrofe Este objeto recibe los parametros propios de una catastrofe y se encarga de manejar el funcionamiento del mapa asignado

--- static/static/js/map.js
+++ static/static/js/map.js
@@ -54,124 +54,147 @@ var greenIcon = L.icon({
     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+var iconos = [yellowIcon, greenIcon, redIcon, orangeIcon, purpleIcon];
+
+function MapHandler(categorias, marcadores, latitud, longitud, mapId) {
+    this.categorias = categorias;
+    this.marcadores = marcadores;
+    this.latitud = latitud;
+    this.longitud = longitud;
+    this.mapId = mapId;
+    this.loadMap = function(){
+
+        //centrar
+        jQuery.fn.center = function() {
+            this.css("position", "absolute");
+            this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 3) +
+                $(window).scrollTop()) + "px");
+            this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+                $(window).scrollLeft()) + "px");
+            return this;
+        }
+
+
+        //block screen
+        jQuery.fn.block = function(){
+            $("#screen").css({
+                "display": 'block',
+                opacity: 0.7,
+                'width': $(document).width(),
+                'height': $(document).height()
+            });
+            $('body').css({
+                'overflow': 'hidden'
+            });
+        }
+
+        //form
+        $("#dialog").css('box-shadow', '0px 0px 2px 3px #000')
+            .css('height', $(".content-dialog").height())
+            .css('z-index', 1001)
+            .hide();
+
+        $("#cerrar").click(
+            function() {
+
+                $("#dialog").fadeOut(400, function() {
+                    $(window).css('display', 'none');
+                    $('#screen').css('display', 'none');
+                });
+
+            }
+        );
+
+        
+
+        //crear mapa
+        var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                maxZoom: 18,
+                //attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
+            }),
+            latlng = L.latLng(this.latitud, this.longitud);
+
+        var map = L.map(this.mapId, {
+            center: latlng,
+            zoom: 15,
+            layers: [tiles]
+        });
+
+        //marcadores
+        var markers = L.markerClusterGroup();
+
+
+        
+        function cargarTodos(marcadores){
+            markers.clearLayers();
+            for (var i = 0; i < marcadores.length; i++) {
+                for(var j = 0; j < marcadores[i].length; j++){
+                    var markerAux = L.marker([marcadores[i][j].fields.latitud, marcadores[i][j].fields.longitud], {
+                        icon: iconos[i]
+                    });
+                    markerAux.bindPopup(marcadores[i][j].fields.description);
+                    markers.addLayer(markerAux);
+                }
+            }    
+        }
+        
+        cargarTodos(this.marcadores);
+
+        map.addLayer(markers);
+
+        //mostrar dialogo
+        var estaPresionado = false;
+        var mostrarDialogo = function(e) {
+            $("#latitud").val(e.latlng.lat);
+            $("#longitud").val(e.latlng.lng);
+            jQuery.fn.block();
+            $("#dialog").center()
+                .fadeIn();
+            estaPresionado = false;
+        }
+
+        //eventos para crear marcador
+
+
+        map.on('mousedown', function(e) {
+            estaPresionado = true;
+            clearTimeout(this.downTimer);
+            this.downTimer = setTimeout(mostrarDialogo, 1000, e);
+
+        });
+
+        map.on('mouseup', function(e) {
+            estaPresionado = false;
+            clearTimeout(this.downTimer);
+
+        });
+
+        //mousemove event
+
+        $(map).mousemove(function(event) {
+            clearTimeout(this.downTimer);
+            this.downTimer = setTimeout(1000);
+            if (estaPresionado) {
+                this.downTimer = setTimeout(mostrarDialogo, 1000);
+            }
+        });
+
+    }
+}
+
 
 function init_map(json_str) {
 
-    var iconos = [yellowIcon, greenIcon, redIcon, orangeIcon, purpleIcon];
+    var mapHandler = new MapHandler(
+            "",
+            jQuery.parseJSON(json_str),
+            -41.77, 
+            -73.134,
+            "map"
+        )
 
-	//centrar
-    jQuery.fn.center = function() {
-        this.css("position", "absolute");
-        this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 3) +
-            $(window).scrollTop()) + "px");
-        this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
-            $(window).scrollLeft()) + "px");
-        return this;
-    }
-
-
-    //block screen
-    jQuery.fn.block = function(){
-        $("#screen").css({
-            "display": 'block',
-            opacity: 0.7,
-            'width': $(document).width(),
-            'height': $(document).height()
-        });
-        $('body').css({
-            'overflow': 'hidden'
-        });
-    }
-
-    //form
-    $("#dialog").css('box-shadow', '0px 0px 2px 3px #000')
-        .css('height', $(".content-dialog").height())
-        .css('z-index', 1001)
-        .hide();
-
-    $("#cerrar").click(
-        function() {
-
-            $("#dialog").fadeOut(400, function() {
-                $(window).css('display', 'none');
-                $('#screen').css('display', 'none');
-            });
-
-        }
-    );
-
-    //crear mapa
-    var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            //attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
-        }),
-        latlng = L.latLng(-41.77, -73.134);
-
-    var map = L.map('map', {
-        center: latlng,
-        zoom: 15,
-        layers: [tiles]
-    });
-
-    //marcadores
-    var markers = L.markerClusterGroup();
-
-
-    var marcadores = jQuery.parseJSON(json_str);
-    function cargarTodos(){
-        markers.clearLayers();
-        for (var i = 0; i < marcadores.length; i++) {
-            for(var j = 0; j < marcadores[i].length; j++){
-                var markerAux = L.marker([marcadores[i][j].fields.latitud, marcadores[i][j].fields.longitud], {
-                    icon: iconos[i]
-                });
-                markerAux.bindPopup(marcadores[i][j].fields.description);
-                markers.addLayer(markerAux);
-            }
-        }    
-    }
-    
-    cargarTodos();
-
-    map.addLayer(markers);
-
-    //mostrar dialogo
-    var estaPresionado = false;
-    var mostrarDialogo = function(e) {
-        $("#latitud").val(e.latlng.lat);
-        $("#longitud").val(e.latlng.lng);
-        jQuery.fn.block();
-        $("#dialog").center()
-            .fadeIn();
-        estaPresionado = false;
-    }
-
-    //eventos para crear marcador
-
-
-    map.on('mousedown', function(e) {
-        estaPresionado = true;
-        clearTimeout(this.downTimer);
-        this.downTimer = setTimeout(mostrarDialogo, 1000, e);
-
-    });
-
-    map.on('mouseup', function(e) {
-        estaPresionado = false;
-        clearTimeout(this.downTimer);
-
-    });
-
-    //mousemove event
-
-    $(map).mousemove(function(event) {
-        clearTimeout(this.downTimer);
-        this.downTimer = setTimeout(1000);
-        if (estaPresionado) {
-            this.downTimer = setTimeout(mostrarDialogo, 1000);
-        }
-    });
-
+    mapHandler.loadMap();
+	
     //filtrar
     $("button.filtro").click(function(){
         var categoria = $(this).attr('categoria') - 1;
