0af00388af0d311a971f4a48f860ba9aafaf9bee
JoseTapiaG <jose.wt@gmail.com>
2015-06-27 02:16:26 -0300

    Se agrega cathegories a admin y se modifica objeto marks en form

--- maps/admin.py
+++ maps/admin.py
@@ -4,4 +4,5 @@ from .models import *
 
 admin.site.register(Category)
 admin.site.register(Mark)
+admin.site.register(Catastrophes)
 
--- maps/forms.py
+++ maps/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 class MarkForm(forms.Form):
     description = forms.CharField(widget=forms.TextInput, max_length=100)
-    category = forms.ModelChoiceField(queryset=Category.objects.none(), empty_label="Seleccione una categoría", widget=forms.Select(attrs={'class':'form-control input-sm','id': 'category-select-list'}))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Seleccione una categoría", widget=forms.Select(attrs={'class':'form-control input-sm','id': 'category-select-list'}))
     latitud = forms.FloatField(widget=forms.TextInput(attrs={'id': 'latitud','type': 'hidden'}))
     longitud = forms.FloatField(widget=forms.TextInput(attrs={'id': 'longitud','type': 'hidden'}))
     idmap = forms.CharField(widget=forms.TextInput(attrs={'id': 'id-map', 'type': 'hidden'}))
